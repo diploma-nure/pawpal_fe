c0f16a6f96232a4d63737b6671c250aef5187426
Max Bobryk <booobryyyk@gmail.com>
2025-03-05 21:26:49 +0200

    add select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@/components/Button/Button';
+import { MultiSelect } from '@/components/Select/Select';
 import styles from './page.module.scss';
 
 export default function Home() {
+  const sortOptions = [
+    'За датою додавання (спочатку нові додані)',
+    'За віком (спочатку молодші)',
+    'За розміром (спочатку маленькі)',
+    'За розміром (спочатку великі)',
+  ];
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Button>Click</Button>
+        <div style={{ maxWidth: '300px' }}>
+          <MultiSelect options={sortOptions} />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React, { useState } from 'react';
+import styles from './styles.module.scss';
+
+interface MultiSelectProps {
+  options: string[];
+  placeholder?: string;
+}
+
+export const MultiSelect: React.FC<MultiSelectProps> = ({
+  options,
+  placeholder = 'Сортувати за',
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleOptionToggle = (option: string) => {
+    setSelectedOptions((prev) =>
+      prev.includes(option)
+        ? prev.filter((item) => item !== option)
+        : [...prev, option],
+    );
+  };
+
+  return (
+    <div className={styles.multiSelectWrapper}>
+      <div className={styles.selectHeader} onClick={toggleDropdown}>
+        <p className={styles.selectedOption}>
+          {selectedOptions.length > 0
+            ? selectedOptions.join(', ')
+            : placeholder}
+        </p>
+        <span className={styles.arrow}></span>
+      </div>
+
+      {isOpen && (
+        <div className={styles.optionsList}>
+          {options.map((option) => (
+            <label key={option} className={styles.checkboxLabel}>
+              <input
+                type="checkbox"
+                checked={selectedOptions.includes(option)}
+                onChange={() => handleOptionToggle(option)}
+              />
+              <p className={styles.checkboxText}>{option}</p>
+            </label>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
