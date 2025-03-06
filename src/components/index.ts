b2cfde2f603b2847a8fe9e642dd58ab665a5758c
Max Bobryk <booobryyyk@gmail.com>
2025-03-06 22:43:14 +0200

    add container component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Button/Button';
 export * from './Checkbox/Checkbox';
+export * from './Container/Container';
 export * from './Icon/Icon';
 export * from './RadioGroup/RadioGroup';
 export * from './Select/Select';
