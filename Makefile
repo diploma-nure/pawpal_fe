619b73afbfb6a97d21f79877317d6dda78011369
Max Bobryk <booobryyyk@gmail.com>
2025-03-06 22:44:57 +0200

    change svg post-process script

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 sprites:
-	npx svg-symbol-sprite -i ./src/assets/icons/svgs -o ./src/assets/icons/sprite.svg -a "xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' width='24' height='24'" && npm run process:svg
+	npm run process:svg && npx svg-symbol-sprite -i ./src/assets/icons/svgs -o ./src/assets/icons/sprite.svg -a "xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' width='24' height='24'"
 run-frontend:
 	npm run dev
--- scripts/post-process.js
+++ scripts/post-process.js
@@ -1,49 +1,79 @@
 import fs from 'fs';
+import path from 'path';
 import xml2js from 'xml2js';
 
-const inputFile = './src/assets/icons/sprite.svg';
-const outputFile = inputFile;
+const inputDir = './src/assets/icons/svgs';
 
-fs.readFile(inputFile, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
-
-  xml2js.parseString(data, (err, result) => {
+const processSvgFile = (filePath) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error('Error parsing the SVG:', err);
+      console.error(`Error reading the file ${filePath}:`, err);
       return;
     }
 
-    const updateFillAttributes = (element) => {
-      if (element.$ && element.$.fill) {
-        element.$.fill = 'inherit';
-      }
-      if (element.$$) {
-        element.$$.forEach((child) => updateFillAttributes(child));
-      }
-    };
-
-    if (result.svg && result.svg.symbol) {
-      result.svg.symbol.forEach((symbol) => {
-        if (symbol.path) {
-          symbol.path.forEach((path) => updateFillAttributes(path));
-        }
-      });
-    }
-
-    const builder = new xml2js.Builder();
-    const updatedSvg = builder.buildObject(result);
-
-    fs.writeFile(outputFile, updatedSvg, (err) => {
+    xml2js.parseString(data, (err, result) => {
       if (err) {
-        console.error('Error writing the file:', err);
+        console.error(`Error parsing the SVG ${filePath}:`, err);
         return;
       }
-      console.log(
-        `SVG file processed successfully. Output saved to ${outputFile}`,
-      );
+
+      const updateFillAttributes = (element) => {
+        if (element.$ && element.$.fill) {
+          element.$.fill = 'inherit';
+        }
+        if (element.$$) {
+          element.$$.forEach((child) => updateFillAttributes(child));
+        }
+      };
+
+      if (result.svg) {
+        if (result.svg.symbol) {
+          result.svg.symbol.forEach((symbol) => {
+            if (symbol.path) {
+              symbol.path.forEach((path) => updateFillAttributes(path));
+            }
+          });
+        }
+        if (result.svg.path) {
+          result.svg.path.forEach((path) => updateFillAttributes(path));
+        }
+      }
+
+      const builder = new xml2js.Builder();
+      const updatedSvg = builder.buildObject(result);
+
+      fs.writeFile(filePath, updatedSvg, (err) => {
+        if (err) {
+          console.error(`Error writing the file ${filePath}:`, err);
+          return;
+        }
+        console.log(`SVG file processed successfully: ${filePath}`);
+      });
     });
+  });
+};
+
+fs.readdir(inputDir, (err, files) => {
+  if (err) {
+    console.error('Error reading the directory:', err);
+    return;
+  }
+
+  const svgFiles = files.filter(
+    (file) => path.extname(file).toLowerCase() === '.svg',
+  );
+
+  if (svgFiles.length === 0) {
+    console.log('No SVG files found in the directory.');
+    return;
+  }
+
+  svgFiles.forEach((file) => {
+    const filePath = path.join(inputDir, file);
+    if (filePath.includes('logo.svg')) {
+      console.log(`Skip reading the file ${filePath}:`);
+      return;
+    }
+    processSvgFile(filePath);
   });
 });
