293a3e41e0f9a031f59f27caf99ade560d93e343
Max Bobryk <booobryyyk@gmail.com>
2025-03-27 01:27:36 +0200

    add sign-up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from '@/features/sign-up/components/SignUpForm/SignUpForm';
+
+export default function SignUp() {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+}
